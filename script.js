6d083517cc15d9eddc73b895cf94ce84a95d6188
cwilliams0926 <coopwilliams03@hotmail.com>
2025-04-02 10:35:49 -0400

    Declare functions for the button presses

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 console.log("File begin");
 
 let firstName = "Johnny";
-let birthYear = "1995";
-let currentYear = "2025";
+let birthYear = 1995;
+let currentYear = 2025;
+
+function fillInBlanks() {
+    console.log("fillInBlanks begin");
+
+
+
+    console.log("fillInBlanks end");
+}
+
+function computeAge() {
+    console.log("computeAge begin");
+
+
+
+    console.log("computeAge end")
+}
+
+function changeGlobals() {
+    console.log("changeGlobals begin");
+
+
+
+    console.log("changeGlobals end");
+}
 
 console.log("File end");
