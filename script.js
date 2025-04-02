1edbaee89f0ec0ec7e5d8765e28787923791b770
cwilliams0926 <coopwilliams03@hotmail.com>
2025-04-02 11:45:33 -0400

    Add button event listener to call fillInBlanks

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ let firstName = "Johnny";
 let birthYear = 1995;
 let currentYear = 2025;
 
-
+// button to fill in blanks
+const button1 = document.querySelector("#btn1");
+button1.addEventListener("click", fillInBlanks);
 
 function fillInBlanks() {
     console.log("fillInBlanks begin");
 
-    const nameSpan = document.querySelector(".span1");
+    const nameSpan = document.querySelector("#span1");
     nameSpan.textContent = firstName;
 
-    const yearSpan = document.querySelector(".span2");
+    const yearSpan = document.querySelector("#span2");
     yearSpan.textContent = birthYear;
 
     console.log("fillInBlanks end");
